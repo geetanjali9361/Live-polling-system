6cd761a0cd9507baa453350e3a77573b5ff57123
Geetanjali <geetanjali9361@gmail.com>
2025-09-25 15:03:37 +0530

    updated homepage

--- frontend/live-poll-frontend/src/pages/roleSelect.jsx
+++ frontend/live-poll-frontend/src/pages/roleSelect.jsx
@@ -3,7 +3,7 @@ import { usePoll } from '../context/pollContext';
 
 export default function RoleSelect({ onContinue }) {
   const { setRole } = usePoll();
-  const [tempRole, setTempRole] = useState('student');
+  const [tempRole, setTempRole] = useState('teacher');
 
   return (
 
@@ -24,21 +24,23 @@ export default function RoleSelect({ onContinue }) {
         <p className="subtitle">Please select the role that best describes you to begin using the live polling system.</p>
         
         <div className="role-cards-horizontal">
-          <button
-            className={`role-card ${tempRole === 'student' ? 'active' : ''}`}
-            onClick={() => setTempRole('student')}
-          >
-            <h3>I'm a Student</h3>
-            <p>Answer live polls and instantly see class results.</p>
-          </button>
-          <button
-            className={`role-card ${tempRole === 'teacher' ? 'active' : ''}`}
-            onClick={() => setTempRole('teacher')}
-          >
-            <h3>I'm a Teacher</h3>
-            <p>Ask questions and view student responses in real time.</p>
-          </button>
-        </div>
+  <button
+    className={`role-card ${tempRole === 'teacher' ? 'active' : ''}`}
+    onClick={() => setTempRole('teacher')}
+  >
+    <h3>I'm a Teacher</h3>
+    <p>Ask questions and view student responses in real time.</p>
+  </button>
+
+  <button
+    className={`role-card ${tempRole === 'student' ? 'active' : ''}`}
+    onClick={() => setTempRole('student')}
+  >
+    <h3>I'm a Student</h3>
+    <p>Answer live polls and instantly see class results.</p>
+  </button>
+</div>
+
         
         <button className="primary" onClick={() => { setRole(tempRole); onContinue(); }}>
           Continue
